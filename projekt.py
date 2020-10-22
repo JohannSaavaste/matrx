e0d2fad087df911d19f22e5ee2dc09f916e4384b
RookieSoldat <62138072+RookieSoldat@users.noreply.github.com>
2020-10-22 22:05:37 +0300

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -1,18 +1,24 @@
-def determinant(maatriks):
-    jar=[]
-    f=open(maatriks, "r")
-    for x in f:
-        x=x.strip().split(" ")
-        yl=[]
-        for y in x:
-            y= float(y)
-            yl.append(y)
-        jar.append(yl)
-    if len(yl)!=len(jar):
-        return "Maatriksist determinanti ei saa võtta"
-    print(jar)
-    f.close()
-    for x in range(len(jar)):
-        diagonaal=0
-        #for y in range(len(x)):
-         #   if 
+kask=input("Programmi käsud: +, -, *, D, T")
+if kask=="+":
+    f1=input("Sisesta esimese maatriksi tekstidokumendi nimi:")
+    f2=input("Sisesta teise maatriksi tekstidokumendi nimi:")
+    m1,m2=tagasta_maatriks(f1), tagasta_maatriks(f2)
+    uus_maatriks=liida(m1, m2)
+elif kask=="-":
+    f1=input("Sisesta esimese maatriksi tekstidokumendi nimi:")
+    f2=input("Sisesta teise maatriksi tekstidokumendi nimi:")
+    m1,m2=tagasta_maatriks(f1), tagasta_maatriks(f2)
+    uus_maatriks=lahuta(m1, m2)
+elif kask=="*":
+    f1=input("Sisesta esimese maatriksi tekstidokumendi nimi:")
+    f2=input("Sisesta teise maatriksi tekstidokumendi nimi:")
+    m1,m2=tagasta_maatriks(f1), tagasta_maatriks(f2)
+    uus_maatriks=korruta(m1, m2)
+elif kask=="D":
+    f=input("Sisesta maatriksi tekstidokumendi nimi:")
+    m=tagasta_maatriks(f)
+    determinant=determinant(m)
+elif kask=="T":
+    f=input("Sisesta maatriksi tekstidokumendi nimi:")
+    m=tagasta_maatriks(f)
+    uus_maatriks=transponeeri(m)
