7443562f8da6c3fdd412dbcc0d40d5283a75458b
RookieSoldat <62138072+RookieSoldat@users.noreply.github.com>
2020-11-10 11:26:46 +0200

    Update transponeeri.py

--- transponeeri.py
+++ transponeeri.py
@@ -1,20 +1,8 @@
 def transponeeri(maatriks):
-    f=open(maatriks, "r")
-    jar=[]
-    for x in f:
-        x=x.strip().split(" ")
-        jar.append(x)
-
     trans=[]
-    for x in range(len(jar)):
+    for x in range(len(maatriks)):
         rida=[]
-        for y in jar:
+        for y in maatriks:
             rida.append(y[x])
         trans.append(rida)
-
-    for x in trans:
-        valjund=""
-        for y in x:
-            valjund+=y
-        valjund+="\n"
-        print(valjund)
+    return trans
