92e41710f38f9019cfc29753875e7496d09bf50d
RookieSoldat <62138072+RookieSoldat@users.noreply.github.com>
2020-11-11 17:30:24 +0200

    Update transponeeri.py

--- transponeeri.py
+++ transponeeri.py
@@ -1,8 +1,9 @@
 def transponeeri(maatriks):
     trans=[]
-    for x in range(len(maatriks)):
-        rida=[]
-        for y in maatriks:
-            rida.append(y[x])
-        trans.append(rida)
-    return trans
+    for z in maatriks:    
+        for x in range(len(z)):
+            rida=[]
+            for y in maatriks:
+                rida.append(y[x])
+            trans.append(rida)
+        return trans
